4dc66fbf2707bf65e3897465a66f642214fc57bf
vdartacademy3 <vdartacademy3@gmail.com>
2026-02-05 12:58:18 +0530

    folders deleted

--- Sum.py
+++
@@ -1,5 +0,0 @@
-def sum(a,b):
-    print(a+b)
-a=int(input("Enter a number : "))
-b=int(input("Enter a number : "))
-sum(a,b)
--- voting.py
+++
@@ -1,5 +0,0 @@
-age=int(input("Enter your age"))
-if age>=18:
-    print("you're eligible to vote")
-else:
-    print("you're not eligible to vote")
